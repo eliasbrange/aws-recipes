5e6a2fac3b076f9bad0bacaba6caa3cbd0d13465
Elias Brange <elias.brange@gmail.com>
2022-07-07 20:22:55 +0200

    add cdk templates

---
+++ decoupled-infrastructure/cdk/export/test/export.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Export from '../lib/export-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/export-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Export.ExportStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
