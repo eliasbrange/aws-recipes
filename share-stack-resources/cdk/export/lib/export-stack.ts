35cfe9c84a44864b47661d856959eb3f25a6ee40
Elias Brange <elias.brange@gmail.com>
2022-07-09 12:00:14 +0200

    fix

--- share-stack-resources/cdk/export/lib/export-stack.ts
+++ share-stack-resources/cdk/export/lib/export-stack.ts
@@ -4,20 +4,20 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 export class ExportStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        const table = new dynamodb.Table(this, 'Table', {
-            partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
-            billingMode: dynamodb.BillingMode.PROVISIONED,
-        });
+    const table = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+    });
 
-        new ssm.StringParameter(this, 'SSMParam', {
-            parameterName: '/cdk/tableName',
-            type: ssm.ParameterType.STRING,
-            stringValue: table.tableName,
-        });
+    new ssm.StringParameter(this, 'SSMParam', {
+      parameterName: '/cdk/tableName',
+      type: ssm.ParameterType.STRING,
+      stringValue: table.tableName,
+    });
 
-        new CfnOutput(this, 'TableNameOutput', { value: table.tableName, exportName: 'CDKExportedTableName' });
-    }
+    new CfnOutput(this, 'TableNameOutput', { value: table.tableName, exportName: 'CDKExportedTableName' });
+  }
 }
--- share-stack-resources/cdk/import/lib/import-stack.ts
+++ share-stack-resources/cdk/import/lib/import-stack.ts
@@ -6,31 +6,31 @@ import * as apigwv2 from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 
 export class ImportStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        const tableNameFromOutput = Fn.importValue('CDKExportedTableName')
-        const tableNameFromSSM = ssm.StringParameter.valueForStringParameter(this, '/cdk/tableName');
+    const tableNameFromOutput = Fn.importValue('CDKExportedTableName');
+    const tableNameFromSSM = ssm.StringParameter.valueForStringParameter(this, '/cdk/tableName');
 
-        const fn = new lambda.NodejsFunction(this, 'MyFunction', {
-            entry: 'lib/app.ts',
-            depsLockFilePath: 'yarn.lock',
-            handler: 'lambdaHandler',
-            environment: {
-                TABLE_NAME_FROM_OUTPUT: tableNameFromOutput,
-                TABLE_NAME_FROM_SSM: tableNameFromSSM,
-            },
-        });
+    const fn = new lambda.NodejsFunction(this, 'MyFunction', {
+      entry: 'lib/app.ts',
+      depsLockFilePath: 'yarn.lock',
+      handler: 'lambdaHandler',
+      environment: {
+        TABLE_NAME_FROM_OUTPUT: tableNameFromOutput,
+        TABLE_NAME_FROM_SSM: tableNameFromSSM,
+      },
+    });
 
-        const param = ssm.StringParameter.fromStringParameterName(this, 'TableNameParam', '/cdk/tableName');
-        param.grantRead(fn);
+    const param = ssm.StringParameter.fromStringParameterName(this, 'TableNameParam', '/cdk/tableName');
+    param.grantRead(fn);
 
-        const httpApi = new apigwv2.HttpApi(this, 'HttpApi');
-        const integration = new HttpLambdaIntegration('Integration', fn);
-        httpApi.addRoutes({
-            path: '/hello',
-            methods: [apigwv2.HttpMethod.GET],
-            integration: integration,
-        });
-    }
+    const httpApi = new apigwv2.HttpApi(this, 'HttpApi');
+    const integration = new HttpLambdaIntegration('Integration', fn);
+    httpApi.addRoutes({
+      path: '/hello',
+      methods: [apigwv2.HttpMethod.GET],
+      integration: integration,
+    });
+  }
 }
