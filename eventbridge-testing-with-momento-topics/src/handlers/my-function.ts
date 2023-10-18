f28ecb693f35bbe0ca5e35dc21963de27923c926
Elias brange <elias.brange@gmail.com>
2023-10-18 19:36:01 +0200

    update handler

--- eventbridge-testing-with-momento-topics/src/handlers/my-function.ts
+++ eventbridge-testing-with-momento-topics/src/handlers/my-function.ts
@@ -1,8 +1,25 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { EventBridgeClient, PutEventsCommand } from '@aws-sdk/client-eventbridge';
 
+const eventBridgeClient = new EventBridgeClient({});
+const { EVENT_BUS } = process.env;
+
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
+    await eventBridgeClient.send(
+      new PutEventsCommand({
+        Entries: [
+          {
+            EventBusName: EVENT_BUS,
+            Source: 'my-source',
+            DetailType: 'my-detail-type',
+            Detail: JSON.stringify({
+              message: 'hello world',
+            }),
+          },
+        ],
+      }),
+    );
     return {
       statusCode: 200,
       body: JSON.stringify({
