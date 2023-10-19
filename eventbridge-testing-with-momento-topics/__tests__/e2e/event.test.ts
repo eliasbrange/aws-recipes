78fd00776e4fab73af98c2706a25046b8a8c8627
Elias Brange <elias.brange@gmail.com>
2023-10-19 18:15:10 +0200

    added e2e test using momento topics (#6)

---
+++ eventbridge-testing-with-momento-topics/__tests__/e2e/event.test.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import { subscribe } from '../lib/subscribe';
+
+const API_URL = process.env.API_URL || '';
+
+describe('When an order is created', async () => {
+  const subscription = await subscribe();
+  let order: { id: string; name: string };
+
+  beforeAll(async () => {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+    });
+    expect(response.status).toBe(201);
+
+    order = await response.json();
+  });
+
+  afterAll(async () => {
+    subscription.unsubscribe();
+  });
+
+  it('It should publish an OrderCreated event to EventBridge', async () => {
+    const message = await subscription.waitForMessageMatching({
+      source: 'OrderService',
+      'detail-type': 'OrderCreated',
+      detail: {
+        id: order.id,
+        name: order.name,
+      },
+    });
+
+    expect(message).not.toBeNull();
+  });
+});
---
+++ eventbridge-testing-with-momento-topics/__tests__/lib/subscribe.ts
@@ -0,0 +1,61 @@
+import 'dotenv/config';
+import { TopicClient, TopicConfigurations, CredentialProvider, TopicItem, TopicSubscribe } from '@gomomento/sdk';
+import { ReplaySubject, firstValueFrom } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import * as _ from 'lodash';
+
+const topicClient = new TopicClient({
+  configuration: TopicConfigurations.Default.latest(),
+  credentialProvider: CredentialProvider.fromEnvironmentVariable({
+    environmentVariableName: 'MOMENTO_TOKEN',
+  }),
+});
+const CACHE_NAME = process.env.CACHE_NAME || '';
+const TOPIC_NAME = process.env.TOPIC_NAME || '';
+
+export const subscribe = async () => {
+  const messages = new ReplaySubject(100);
+
+  console.log('Subscribing to Momento Topic');
+  const subscription = await topicClient.subscribe(CACHE_NAME, TOPIC_NAME, {
+    onError: (error) => {
+      throw error;
+    },
+    onItem: (item: TopicItem) => {
+      try {
+        const message = JSON.parse(item.valueString());
+        console.log('Received message from Momento Topic', message);
+        messages.next(message);
+      } catch (error) {
+        console.log('Error parsing message from Momento Topic', item);
+      }
+    },
+  });
+
+  if (!(subscription instanceof TopicSubscribe.Subscription)) {
+    throw new Error('Failed to subscribe to topic');
+  }
+
+  const unsubscribe = async () => {
+    console.log('Unsubscribing to Momento Topic');
+    subscription.unsubscribe();
+  };
+
+  const waitForMessageMatching = async (expected: object) => {
+    const predicate = (message: unknown) => {
+      if (typeof message !== 'object' || message === null) {
+        return false;
+      }
+
+      return _.isMatch(message, expected);
+    };
+
+    const data = messages.pipe(filter((message) => predicate(message)));
+    return firstValueFrom(data);
+  };
+
+  return {
+    unsubscribe,
+    waitForMessageMatching,
+  };
+};
