dc5951d86b5affbd357896f8510953d21a9c7096
Elias brange <elias.brange@gmail.com>
2024-11-29 19:07:09 +0100

    refactor test resources into a construct

--- eventbridge-testing-with-appsync-events/lib/eventbridge-testing-with-appsync-events-stack.ts
+++ eventbridge-testing-with-appsync-events/lib/eventbridge-testing-with-appsync-events-stack.ts
@@ -1,11 +1,10 @@
 import * as cdk from "aws-cdk-lib";
 import * as eventbridge from "aws-cdk-lib/aws-events";
-import * as eventbridgeTargets from "aws-cdk-lib/aws-events-targets";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
-import * as appsync from "aws-cdk-lib/aws-appsync";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import type { Construct } from "constructs";
+import { TestResources } from "./test-resources";
 
 interface StackProps extends cdk.StackProps {
   includeTestStack?: boolean;
@@ -34,69 +33,7 @@ export class EventbridgeTestingWithAppsyncEventsStack extends cdk.Stack {
       .addMethod("POST", new apigateway.LambdaIntegration(apiFn));
 
     if (props?.includeTestStack) {
-      createTestResources(this, eventBus);
+      new TestResources(this, "TestResources", { eventBus });
     }
   }
 }
-
-const createTestResources = (
-  scope: cdk.Stack,
-  eventBus: eventbridge.IEventBus,
-) => {
-  const eventsApi = new appsync.CfnApi(scope, "TestEventsApi", {
-    name: "TestEventsApi",
-    eventConfig: {
-      authProviders: [
-        {
-          authType: "API_KEY",
-        },
-      ],
-      connectionAuthModes: [
-        {
-          authType: "API_KEY",
-        },
-      ],
-      defaultPublishAuthModes: [
-        {
-          authType: "API_KEY",
-        },
-      ],
-      defaultSubscribeAuthModes: [
-        {
-          authType: "API_KEY",
-        },
-      ],
-    },
-  });
-
-  const eventsApiKey = new appsync.CfnApiKey(scope, "TestEventsApiKey", {
-    apiId: eventsApi.attrApiId,
-  });
-
-  new appsync.CfnChannelNamespace(scope, "TestEventsNamespace", {
-    apiId: eventsApi.attrApiId,
-    name: "default",
-  });
-
-  const publishToAppSyncFn = new NodejsFunction(
-    scope,
-    "PublishToAppSyncFunction",
-    {
-      entry: "src/publishToAppSync.ts",
-      handler: "handler",
-      runtime: lambda.Runtime.NODEJS_20_X,
-      environment: {
-        EVENTS_API_URL: `https://${eventsApi.attrDnsHttp}`,
-        EVENTS_API_KEY: eventsApiKey.attrApiKey,
-      },
-    },
-  );
-
-  new eventbridge.Rule(scope, "TestEventsRule", {
-    eventBus,
-    eventPattern: {
-      source: eventbridge.Match.prefix(""),
-    },
-    targets: [new eventbridgeTargets.LambdaFunction(publishToAppSyncFn)],
-  });
-};
---
+++ eventbridge-testing-with-appsync-events/lib/test-resources.ts
@@ -0,0 +1,73 @@
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as appsync from "aws-cdk-lib/aws-appsync";
+import * as eventbridge from "aws-cdk-lib/aws-events";
+import * as eventbridgeTargets from "aws-cdk-lib/aws-events-targets";
+import { Construct } from "constructs";
+
+interface ConstructProps {
+  eventBus: eventbridge.IEventBus;
+}
+
+export class TestResources extends Construct {
+  constructor(scope: Construct, id: string, props: ConstructProps) {
+    super(scope, id);
+
+    const eventsApi = new appsync.CfnApi(this, "TestEventsApi", {
+      name: "TestEventsApi",
+      eventConfig: {
+        authProviders: [
+          {
+            authType: "API_KEY",
+          },
+        ],
+        connectionAuthModes: [
+          {
+            authType: "API_KEY",
+          },
+        ],
+        defaultPublishAuthModes: [
+          {
+            authType: "API_KEY",
+          },
+        ],
+        defaultSubscribeAuthModes: [
+          {
+            authType: "API_KEY",
+          },
+        ],
+      },
+    });
+
+    const eventsApiKey = new appsync.CfnApiKey(this, "TestEventsApiKey", {
+      apiId: eventsApi.attrApiId,
+    });
+
+    new appsync.CfnChannelNamespace(this, "TestEventsNamespace", {
+      apiId: eventsApi.attrApiId,
+      name: "default",
+    });
+
+    const publishToAppSyncFn = new NodejsFunction(
+      this,
+      "PublishToAppSyncFunction",
+      {
+        entry: "src/publishToAppSync.ts",
+        handler: "handler",
+        runtime: lambda.Runtime.NODEJS_20_X,
+        environment: {
+          EVENTS_API_URL: `https://${eventsApi.attrDnsHttp}`,
+          EVENTS_API_KEY: eventsApiKey.attrApiKey,
+        },
+      },
+    );
+
+    new eventbridge.Rule(this, "TestEventsRule", {
+      eventBus: props.eventBus,
+      eventPattern: {
+        source: eventbridge.Match.prefix(""),
+      },
+      targets: [new eventbridgeTargets.LambdaFunction(publishToAppSyncFn)],
+    });
+  }
+}
